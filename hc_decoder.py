f3242cb445cd1687c0bdfb0ce1cf29eb5c370f19
ahottung <andre.hottung@uni-bielefeld.de>
2021-07-14 18:33:43 +0200

    Clean-up

--- hc_decoder.py
+++
@@ -1,8 +0,0 @@
-import torch
-from tsp import tours_length
-
-def argsort_decode(Z, decoder, config, instance, tour):
-    Z = torch.Tensor(Z).to(config.device)
-    tour_idx = torch.argsort(Z, dim=1)
-    costs = tours_length(instance.permute(0, 2, 1), tour_idx)
-    return tour_idx, costs.tolist()
